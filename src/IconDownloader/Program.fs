c615c30119793f3329041fdde8ca12b422539ebc
AOKI Takashi <55625375+RyushiAok@users.noreply.github.com>
2023-09-09 22:17:32 +0900

    feat: output path

--- src/IconDownloader/Program.fs
+++ src/IconDownloader/Program.fs
@@ -1,68 +1,86 @@
 ﻿open FsHttp
 open System.IO
 
-let SAVE_PATH = $"{__SOURCE_DIRECTORY__}/../../tmp"
-
-let downloadIcon url fileName =
+let downloadIcon url (filePath: string) =
     http { GET url }
     |> Request.send
     |> Response.toStringAsync None
     |> Async.RunSynchronously
-    |> fun svg -> File.WriteAllText(fileName, svg)
+    |> fun svg ->
+        let dir = Path.GetDirectoryName(filePath)
 
-let getDefaultIconURL name =
-    $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/wght200/24px.svg"
+        if not <| Directory.Exists(dir) then
+            Directory.CreateDirectory(dir) |> ignore
 
-let getBoldIconURL name =
-    $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/default/24px.svg"
+        File.WriteAllText(filePath, svg)
 
-let getFilledIconURL name =
-    $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/fill1/24px.svg"
 
-open Argu
-
+/// default
+/// - weight 200 rounded 24px
+/// - ex. https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/search/wght200/24px.svg
+///
+/// bold
+/// - weight 400 rounded 24px
+/// - ex. https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/search/default/24px.svg
+///
+/// filled
+/// - ex. https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/favorite/fill1/24px.svg
 type IconStyle =
     | Default
     | Bold
     | Filled
 
+let getIconURL style name =
+    match style with
+    | Default -> $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/wght200/24px.svg"
+    | Bold -> $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/default/24px.svg"
+    | Filled -> $"https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/{name}/fill1/24px.svg"
+
+open Argu
+
 type Args =
     | Name of string
     | Style of IconStyle
+    | Out of string
 
     interface IArgParserTemplate with
         member s.Usage =
             match s with
             | Name _ -> "specify icon name"
             | Style _ -> "specify icon style"
+            | Out _ -> "specify output path"
 
 [<EntryPoint>]
 let main argv =
-    let parser = ArgumentParser.Create<Args>(programName = "icon downloader")
-    let parseResults = parser.ParseCommandLine argv
-    printfn "%A" parseResults
 
-    let url, fileName =
+    let parseResults =
+        let parser = ArgumentParser.Create<Args>(programName = "icon downloader")
+        parser.ParseCommandLine argv
+
+    let url, filePath =
         let name = parseResults.GetResult Name
         let style = parseResults.TryGetResult Style |> Option.defaultValue Default
 
-        match style with
-        | Default -> getDefaultIconURL name, name
-        | Bold -> getBoldIconURL name, $"{name}_bold"
-        | Filled -> getFilledIconURL name, $"{name}_filled"
+        let outPath =
+            parseResults.TryGetResult Out
+            |> Option.defaultValue $"{__SOURCE_DIRECTORY__}/../../tmp"
 
-    let path = $"{SAVE_PATH}/{fileName}.svg"
-    downloadIcon url path
+
+        printfn $"name: {name}, style: {style}"
+
+        let fileName =
+            match style with
+            | Default -> name
+            | Bold -> $"{name}_bold"
+            | Filled -> $"{name}_filled"
+
+        let filePath = $"{outPath}/{fileName}.svg"
+
+        let url = getIconURL style name
+        printfn $"url: {url}"
+        printfn $"filePath: {filePath}"
+        url, filePath
+
+    downloadIcon url filePath
+
     0
-
-
-// default: weight 200 rounded 24px
-// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/search/wght200/24px.svg
-// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/home/wght200/24px.svg
-
-// bold; weight 400 rounded 24px
-// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/search/default/24px.svg
-
-// filled
-// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/favorite/fill1/24px.svg
-// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsrounded/check_circle/fill1/24px.svg
