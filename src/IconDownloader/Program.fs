71a6ed0a9b4c8ee9ae72642d9edafb6dec701be3
AOKI Takashi <55625375+RyushiAok@users.noreply.github.com>
2023-09-09 15:54:43 +0900

    init

---
+++ src/IconDownloader/Program.fs
@@ -0,0 +1,8 @@
+﻿// https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/add/default/24px.svg
+
+open FsHttp
+
+http { GET "https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/add/default/24px.svg" }
+|> Request.send
+|> Response.saveFile ($"{__SOURCE_DIRECTORY__}/add.svg")
+|> printfn "%A"
